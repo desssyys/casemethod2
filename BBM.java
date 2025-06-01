b52dc4ce3581ee5b0e4dd28ac948a198bc2f457a
unknown <bima.rafi60@sma.belajar.id>
2025-06-01 22:20:48 +0700

    lupa blom push

---
+++ BBM.java
@@ -0,0 +1,14 @@
+public class BBM {
+    String namaBBM;
+    double hargaPerLiter;
+
+    public BBM(String namaBBM, double hargaPerLiter) {
+        this.namaBBM = namaBBM;
+        this.hargaPerLiter = hargaPerLiter;
+    }
+
+    public double getHargaPerLiter() {
+        return hargaPerLiter;
+    }
+}
+
